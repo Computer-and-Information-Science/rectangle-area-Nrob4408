368c012ee73252734b1021463b9457e3fcd40686
Nrob4408 <nate.roberts@student.northampton.edu>
2026-02-01 19:51:52 +0000

    Rectange Assignment Nate Roberts

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//To Do: Nate Roberts
+//To Do: Calculate Rectangle Area and perimeter
+
 #include <iostream>
 using namespace std;
 
@@ -5,5 +8,16 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  
+  double area;
+  int perimeter;  
+  
+  area = length*width;
+perimeter = 2*length + 2*width;
 
+cout << "Rectange Properties: " << endl;
+cout << "Length = " << length << endl;
+cout << "Width = " << width << endl;
+cout << "Area = " << area << endl;
+cout << "Perimeter = " << perimeter << endl;
 }
